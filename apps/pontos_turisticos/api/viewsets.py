5ea44cc63f7907ed3c24adde45007517d4362496
arthurhenrique02 <arthurlimanascimento@hotmail.com>
2023-04-12 14:01:50 -0300

    adicionado recurso de paginação a api de pontos turisticos

--- apps/pontos_turisticos/api/viewsets.py
+++ apps/pontos_turisticos/api/viewsets.py
@@ -1,14 +1,18 @@
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.pagination import LimitOffsetPagination
 
 from apps.pontos_turisticos.models import PontoTuristico
 
 from .serializers import PontoTuristicoSerializer
 
+
 # criar view set
 class PontoTuristicoViewSet(ModelViewSet):
-
     # definir query set (provisoriamente o .all)
     queryset = PontoTuristico.objects.all()
 
     # definir serializer
     serializer_class = PontoTuristicoSerializer
+
+    # adicionar paginação
+    pagination_class = LimitOffsetPagination
