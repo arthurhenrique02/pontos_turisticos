221e55062f5c49642e6fee703e4223bde8132e9f
arthurhenrique02 <arthurlimanascimento@hotmail.com>
2023-04-13 16:26:31 -0300

    sobreescrito a get_query set, definido uma get_queryset simples, provisoriamente

--- apps/pontos_turisticos/api/viewsets.py
+++ apps/pontos_turisticos/api/viewsets.py
@@ -9,10 +9,30 @@ from .serializers import PontoTuristicoSerializer
 # criar view set
 class PontosTuristicosViewSet(ModelViewSet):
     # definir query set (provisoriamente o .all)
-    queryset = PontoTuristico.objects.all()
+    # queryset = PontoTuristico.objects.all()
 
     # definir serializer
     serializer_class = PontoTuristicoSerializer
 
     # adicionar paginação
     pagination_class = LimitOffsetPagination
+
+    # sobreescrever get_queryset
+    def get_queryset(self):
+        # query set padrao = pegar todos os objetos
+        queryset = PontoTuristico.objects.all()
+
+        # queryset para um determinado status
+        # transformar em string e escrever com primeira letra maiuscula (capitalize)
+        status = self.request.query_params.get("status")
+
+        # tratar queryset
+        # if status.is):
+        #     status = "True"
+
+        # mudar queryset
+        if status:
+            queryset = PontoTuristico.objects.filter(status=status)
+
+        # retornar query
+        return queryset
