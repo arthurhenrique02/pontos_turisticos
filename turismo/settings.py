e8afb886940d941f6ea043395542a894ffec630b
arthurhenrique02 <arthurlimanascimento@hotmail.com>
2023-04-21 16:25:28 -0300

    Comentado modificações feitas para o deploy no Heroku e retornado as configurações padrões

--- turismo/settings.py
+++ turismo/settings.py
@@ -1,12 +1,12 @@
 from pathlib import Path
 
-import os
+# import os
 
 # importar python decouple para pegar as informações
-from decouple import config
+# from decouple import config
 
 # django db url para criacao da url pro banco de dados
-from dj_database_url import parse as dburl
+# from dj_database_url import parse as dburl
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -16,11 +16,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config("SECRET_KEY")
+SECRET_KEY = "django-insecure-+!8rg0u7p*#_jl$yaw5j8g8l7*s4-t=&f9e!q^7fp4yns=lqyx"
+# SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
 # passar a variavel de debug, o metodo padrao caso nao ache, e o cast (conversao) para false (isso é para deploy no heroku)
-DEBUG = config("DEBUG", default=False, cast=bool)
+# DEBUG = config("DEBUG", default=False, cast=bool)
 
 ALLOWED_HOSTS = []
 
@@ -77,12 +79,17 @@ WSGI_APPLICATION = "turismo.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
-# utilizando url do db para deploy no heroku
-default_dburl = "sqlite:///" + os.path.join(BASE_DIR, "db.sqlite3")
 DATABASES = {
-    "default": config("DATABASE_URL", default=default_dburl, cast=dburl),
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
 }
+# utilizando url do db para deploy no heroku
+# default_dburl = "sqlite:///" + os.path.join(BASE_DIR, "db.sqlite3")
+# DATABASES = {
+#     "default": config("DATABASE_URL", default=default_dburl, cast=dburl),
+# }
 
 
 # Password validation
@@ -137,4 +144,4 @@ REST_FRAMEWORK = {
 }
 
 
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+# STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
--- turismo/wsgi.py
+++ turismo/wsgi.py
@@ -3,8 +3,10 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 # dj static para o django conseguir enviar as imagens para o server no heroku
-from dj_static import Cling
+# from dj_static import Cling
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "turismo.settings")
 
-application = Cling(get_wsgi_application())
+application = get_wsgi_application()
+
+# application = Cling(get_wsgi_application())
